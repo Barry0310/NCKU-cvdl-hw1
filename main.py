ef1aba882676109683744e4c36523f912197bbec
BarryYan <barry0310yan@gmail.com>
2021-11-15 11:53:59 +0800

    v1.3.1

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.loadImage)
         self.ui.pushButton_2.clicked.connect(self.colorSeperation)
+        self.ui.pushButton_3.clicked.connect(self.colorTransformation)
 
     def loadImage(self):
         name = 'Sun.jpg'
@@ -24,12 +25,16 @@ class MainWindow(QMainWindow):
         pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q1_Image/' + name)
         (B, G, R) = cv2.split(pic)
         zeros = np.zeros(pic.shape[:2], dtype='uint8')
-        print(zeros.shape)
         cv2.imshow('R', cv2.merge([zeros, zeros, R]))
         cv2.imshow('G', cv2.merge([zeros, G, zeros]))
         cv2.imshow('B', cv2.merge([B, zeros, zeros]))
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def colorTransformation(self):
+        name = 'Sun.jpg'
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q1_Image/' + name)
+        cv2.imshow('l1', cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY))
 
 if __name__ == '__main__':
     app = QApplication([])
