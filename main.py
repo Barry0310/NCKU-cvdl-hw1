9b8a6dd9dac08e03f2eee5be6da34a8caafb0f40
BarryYan <barry0310yan@gmail.com>
2021-11-18 17:32:55 +0800

    v4.2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from window import Ui_Myapp
 import cv2
 import numpy as np
+from math import pi, exp
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,11 +17,18 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_5.clicked.connect(self.gaussianBlur)
         self.ui.pushButton_6.clicked.connect(self.bilateralFilter)
         self.ui.pushButton_7.clicked.connect(self.medianFilter)
+        self.ui.pushButton_13.clicked.connect(self.gaussianBlur2)
+        self.ui.pushButton_14.clicked.connect(self.sobelX)
+        self.ui.pushButton_15.clicked.connect(self.sobelY)
+        self.ui.pushButton_16.clicked.connect(self.magnitude)
+        self.ui.pushButton_9.clicked.connect(self.resizee)
+        self.ui.pushButton_10.clicked.connect(self.translation)
 
 
     def loadImage(self):
         pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q1_Image/Sun.jpg')
         cv2.imshow('Sun.jpg', pic)
+        print(pic.shape[:2])
 
     def colorSeperation(self):
         pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q1_Image/Sun.jpg')
@@ -36,7 +44,6 @@ class MainWindow(QMainWindow):
         cv2.imshow('l1', l1)
         (B, G, R) = cv2.split(pic)
         l2 = (R.astype(int) + G.astype(int) + B.astype(int))/3
-        l2 = l2.astype('uint8')
         cv2.imshow('l2', l2.astype('uint8'))
 
     def blending(self):
@@ -73,6 +80,78 @@ class MainWindow(QMainWindow):
         median5 = cv2.medianBlur(pic, 5)
         cv2.imshow('Median Filter 3x3', median3)
         cv2.imshow('Median Filter 5x5', median5)
+
+    def gaussianBlurImplement(self, pic):
+        pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+        result = np.zeros((3, 3))
+        for i in range(3):
+            for j in range(3):
+                result[i][j] = exp(-((i - 1) * (i - 1) + (j - 1) * (j - 1)) / (2 * 0.5)) / (2 * pi * 0.5)
+        all = result.sum()
+        result = result / all
+        pic = pic.astype(float)
+        new = np.zeros(pic.shape)
+        for i in range(1, pic.shape[0] - 1):
+            for j in range(1, pic.shape[1] - 1):
+                t = pic[i - 1:i + 2, j - 1:j + 2]
+                a = np.multiply(t, result)
+                new[i][j] = a.sum()
+        return new.astype('uint8')
+
+    def gaussianBlur2(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q3_Image/House.jpg')
+        cv2.imshow('Gaussian Blur 2', self.gaussianBlurImplement(pic))
+
+    def sobelImplement(self, pic, filter):
+        pic = self.gaussianBlurImplement(pic).astype(float)
+        new = np.zeros(pic.shape)
+        for i in range(1, pic.shape[0] - 1):
+            for j in range(1, pic.shape[1] - 1):
+                t = pic[i - 1:i + 2, j - 1:j + 2]
+                a = np.multiply(filter, t)
+                new[i, j] = a.sum()
+        return np.absolute(new).astype('uint8')
+
+    def sobelX(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q3_Image/House.jpg')
+        filter = np.array([[-1, 0, 1],
+                           [-2, 0, 2],
+                           [-1, 0, 1]])
+        cv2.imshow('Sobel X', self.sobelImplement(pic, filter))
+
+    def sobelY(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q3_Image/House.jpg')
+        filter = np.array([[1, 2, 1],
+                           [0, 0, 0],
+                           [-1, -2, -1]])
+        cv2.imshow('Sobel Y', self.sobelImplement(pic, filter))
+
+    def magnitude(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q3_Image/House.jpg')
+        filter = np.array([[-1, 0, 1],
+                           [-2, 0, 2],
+                           [-1, 0, 1]])
+        x = self.sobelImplement(pic, filter).astype(float)
+        filter = np.array([[1, 2, 1],
+                           [0, 0, 0],
+                           [-1, -2, -1]])
+        y = self.sobelImplement(pic, filter).astype(float)
+        m = np.sqrt(np.square(x)+np.square(y))
+        new = (m*255/m.max()).astype('uint8')
+        cv2.imshow('Magnitude', new)
+
+    def resizee(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q4_Image/SQUARE-01.png')
+        pic = cv2.resize(pic, (256, 256))
+        cv2.imshow('Resize', pic)
+
+    def translation(self):
+        pic = cv2.imread('Dataset_OpenCvDl_Hw1/Q4_Image/SQUARE-01.png')
+        pic = cv2.resize(pic, (256, 256))
+        t = np.float32([[1, 0, 0],
+                        [0, 1, 60]])
+        shifted = cv2.warpAffine(pic, t, (pic.shape[0], pic.shape[1]))
+        cv2.imshow('Translation', shifted)
 
 
 if __name__ == '__main__':
